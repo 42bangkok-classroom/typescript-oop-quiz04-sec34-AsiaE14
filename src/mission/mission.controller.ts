e013941167e3cc55c73385838d0db561a8e81c7f
AsiaE14 <sadgudo@gmail.com>
2026-03-21 17:14:31 +0700

    quiz1-1

--- src/mission/mission.controller.ts
+++ src/mission/mission.controller.ts
@@ -1,17 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { MissionService } from './mission.service';
 
-
 @Controller('missions/summary')
 export class MissionController {
-  constructor(private readonly missionService: MissionService) { }
+  constructor(private readonly missionService: MissionService) {}
 
   @Get()
   getSum(): object {
     return this.missionService.getSummary();
   }
-
-
-
-
 }
